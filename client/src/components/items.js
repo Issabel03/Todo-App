b2ad5ca2623c7b86cbafd5c404291573e9d828f7
Nwoteh Issabel <nwotehissabel@gmail.com>
2022-08-26 08:37:18 +0100

    New Project

---
+++ client/src/components/items.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Item({text, remove, update}) {
+    return (
+        <div className="item">
+        <div className="text">{text} </div>
+        <div className="icons">
+        <i className="ri-pencil-fill" onClick={update}></i>
+        <i className="ri-delete-bin-fill" onClick={remove}></i>
+             
+        </div>
+
+        </div>
+    )
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const cors = require("cors");
+const routes = require("./routes/ToDoRoutes");
+
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+app.use(routes);
+
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Mongodb Connected..."))
+    .catch((err) => console.error(err));
+
+
+app.listen(5000, () => {
+    console.log('App is listening on port 5000!');
+
+})
